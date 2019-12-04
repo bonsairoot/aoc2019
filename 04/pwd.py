822a6386965be3b647f5fd018f4a6329bfe22ae0
gian <g.wetten@bluewin.ch>
2019-12-04 14:32:27 +0100

    Day 4 bugfix + problem text

--- 04/pwd.py
+++ 04/pwd.py
@@ -3,40 +3,34 @@
 from constraint import *
 from collections import Counter
 
-def even_dups(u,v,w,x,y,z):
-    counter = Counter([u,v,w,x,y,z])
-    for v in counter.values():
-        if v % 2 != 0 and v != 1:
-            return False
-    return True
 
 def main():
     with open("aoc04_input.txt") as f:
-        lb,ub = f.read().split('-')
+        lb, ub = f.read().split('-')
 
     problem = Problem()
-    domain = []
     for i in range(6):
         if i > 0:
-            domain = range(min(domain),10)
+            domain = range(min(domain), 10)
             problem.addVariable(i, domain)
-            problem.addConstraint(lambda x,y: y>x or y==x,(i-1,i))
+            problem.addConstraint(lambda x, y: y > x or y == x, (i - 1, i))
         else:
-            domain = range(int(lb[i]),int(ub[i]) + 1)
+            domain = range(int(lb[i]), int(ub[i]) + 1)
             problem.addVariable(i, domain)
 
+    # Part I
+    # problem.addConstraint(lambda u,v,w,x,y,z: u==v or v==w or w==x or x==y or y==z,range(6))
 
-    problem.addConstraint(lambda u,v,w,x,y,z: u==v or v==w or w==x or x==y or y==z,range(6))
-    problem.addConstraint(even_dups ,range(6))
+    # Part II
+    problem.addConstraint(lambda *args: 2 in Counter(args).values(), range(6))
 
     in_bound_solutions = []
     for solution in problem.getSolutions():
-        out = int(''.join([str(v) for k,v in sorted(solution.items())]))
+        out = int(''.join([str(v) for k, v in sorted(solution.items())]))
         if out >= int(lb) and out <= int(ub):
             in_bound_solutions.append(out)
 
     print(len(in_bound_solutions))
-
 
 
 if __name__ == "__main__":
